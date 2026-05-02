0942a8115958f92bf4863ce805c03677f84db00d
Usergnalin <usernilang@gmail.com>
2026-05-02 15:58:31 +0800

    add dotenv-cli for environment variable management and update dev script

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,23 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import babel from '@rolldown/plugin-babel'
+  import { defineConfig } from 'vite'
+  import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+  import tailwindcss from '@tailwindcss/vite'
+  import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  server: {
-    host: '0.0.0.0', // Listen on all network interfaces
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      protocol: 'wss',     // Crucial: Use Secure WebSockets
-      host: 'gnalin.xyz',  // Your domain
-      clientPort: 5174      // The browser should look at the standard SSL port
-    },
-    proxy: {
-      '/api': 'http://localhost:3000'
+  // https://vite.dev/config/
+  export default defineConfig({
+    plugins: [
+      react(),
+      tailwindcss(),
+      babel({ presets: [reactCompilerPreset()] })
+    ],
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      strictPort: true,
+      hmr: {
+        protocol: 'wss', 
+        host: 'gnalin.xyz',
+        clientPort: 443 
+      }
     }
-  }
-})
+  })
